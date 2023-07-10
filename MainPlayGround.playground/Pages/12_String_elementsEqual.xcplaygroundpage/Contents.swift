3c135246c51f84bed62df10aea3c9c2df4bd21d6
fourcade7 <Fourcadeaminov7@gmail.com>
2023-07-11 01:30:37 +0500

    Alhamdulillah equals

---
+++ MainPlayGround.playground/Pages/12_String_elementsEqual.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+ elementsEqual() Syntax
+ The syntax of the string elementsEqual() method is:
+
+ string.elementsEqual(str: String)
+ Here, string is an object of the String class.
+ */
+
+
+
+var str1 = "Learn Swift"
+var str2 = "Learn Java"
+
+// if str1 and str2 are equal, the result is true
+if (str1.elementsEqual(str2)) {
+  print("str1 and str2 are equal")
+}
+else {
+  print("str1 and str2 are not equal")
+}

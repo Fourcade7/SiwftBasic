4fd964ac466779e81529f26011da7ea60481637c
fourcade7 <Fourcadeaminov7@gmail.com>
2023-07-11 01:18:41 +0500

    Elhamdulillah string append

---
+++ MainPlayGround.playground/Pages/10_String_append.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var msg1 = "Swift"
+var msg2 = "Programming"
+
+// append str1 and str2
+msg1.append(msg2)
+print(msg1)
+
+// Output: SwiftProgramming
